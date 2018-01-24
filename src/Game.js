99c0db66e0def6891a99c08c294562bb58523e65
Nick Nedev <nedevn@gmail.com>
2018-01-23 23:06:11 -0800

    Complete

--- src/Game.js
+++ src/Game.js
@@ -10,12 +10,13 @@ class Game extends Component {
       history: [{
         squares: Array(9).fill(null),
       }],
-      xIsNext: true
+      xIsNext: true, 
+      stepNumber: 0,
     }
   }
 
   handleClick = (i) => {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     // calling .slice to copy the contents of the array instead of mutating the existing array
     const squares = current.squares.slice();
@@ -30,14 +31,33 @@ class Game extends Component {
       history: history.concat([{
         squares: squares
       }]),
-      xIsNext: !this.state.xIsNext
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo = (step) => {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+      'Go to move #' + move :
+      'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => {this.jumpTo(move)}}>{desc}</button>
+        </li>
+      )
+    });
 
     let status;
     if (winner) {
@@ -55,7 +75,7 @@ class Game extends Component {
         </div>
         <div className="game-info">
           <div> {status} </div>
-          <ol> {/* TODO */}</ol>
+          <ol> {moves}</ol>
         </div>
       </div>
     )
