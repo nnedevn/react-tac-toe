2b3cccd0263fb111ff54994b77beef884ca33532
Nick Nedev <nedevn@gmail.com>
2018-01-23 22:33:01 -0800

    Fixes logic error

--- src/Board.js
+++ src/Board.js
@@ -83,7 +83,7 @@ function calculateWinner(squares){
 
   for(let i = 0; i<lines.length; i+=1){
     const[a, b, c] = lines[i];
-    if(squares[a] && squares[b] === squares[b] && squares[a] === squares[c]) {
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
